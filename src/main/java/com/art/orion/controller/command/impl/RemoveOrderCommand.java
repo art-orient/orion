4c4f4a074c807ebe5ba04021c7fc0810731a2acf
Aliaksandr Artsikhovich <art-orient@tut.by>
2021-07-13 23:21:54 +0300

    delete non-used imports

--- src/main/java/com/art/orion/controller/command/impl/RemoveOrderCommand.java
+++ src/main/java/com/art/orion/controller/command/impl/RemoveOrderCommand.java
@@ -1,9 +1,7 @@
 package com.art.orion.controller.command.impl;
 
 import com.art.orion.controller.command.Command;
-import com.art.orion.controller.command.util.Paginator;
 import com.art.orion.controller.command.util.RequestParseNumberHelper;
-import com.art.orion.model.entity.Order;
 import com.art.orion.model.service.OrderService;
 import com.art.orion.model.service.ServiceException;
 import com.art.orion.util.ConfigManager;
@@ -11,14 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.List;
-
-import static com.art.orion.controller.command.util.Paginator.LIMIT;
-import static com.art.orion.util.Constant.NUMBER_ORDERS;
-import static com.art.orion.util.Constant.NUMBER_PAGES;
-import static com.art.orion.util.Constant.ORDERS;
-import static com.art.orion.util.Constant.USERNAME;
 
 public class RemoveOrderCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
